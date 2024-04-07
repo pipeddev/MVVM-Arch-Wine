513cd6f05c008391d3840b585e2fff9919220874
Felipe Carrasco <carrascogalindo.felipe@gmail.com>
2024-04-07 16:55:42 -0400

    feature: Set Fragment with viewModel

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     alias(libs.plugins.kotlinSymbolProcessing) apply false
+    alias(libs.plugins.org.jetbrains.kotlin.kapt) apply false
 }
