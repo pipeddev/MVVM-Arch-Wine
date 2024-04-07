ce5bb1c78d787b0a4ee223623b9eadf6b4a7f24b
Felipe Carrasco <carrascogalindo.felipe@gmail.com>
2024-04-07 18:59:17 -0400

    feature: set binding in adapter

--- app/src/main/java/com/pipe/d/dev/mvvmarchwine/favoriteModule/view/WineFavListAdapter.kt
+++ app/src/main/java/com/pipe/d/dev/mvvmarchwine/favoriteModule/view/WineFavListAdapter.kt
@@ -1,7 +1,7 @@
 package com.pipe.d.dev.mvvmarchwine.favoriteModule.view
 
-import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.pipe.d.dev.mvvmarchwine.BR
 import com.pipe.d.dev.mvvmarchwine.homeModule.WineListAdapter
 
 /****
@@ -21,12 +21,6 @@ import com.pipe.d.dev.mvvmarchwine.homeModule.WineListAdapter
 class WineFavListAdapter : WineListAdapter() {
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         super.onBindViewHolder(holder, position)
-        val wine = getItem(position)
-        with((holder as ViewHolder).binding) {
-            cbFavorite.apply {
-                isChecked = wine.isFavorite
-                visibility = View.VISIBLE
-            }
-        }
+        (holder as ViewHolder).binding?.setVariable(BR.isFavModule, true)
     }
 }
