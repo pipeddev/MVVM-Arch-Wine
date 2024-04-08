d5e7c5e1a041bbfbead299a3e0e59f2c3d2b441c
Felipe Carrasco <carrascogalindo.felipe@gmail.com>
2024-04-07 20:53:16 -0400

    feature: set fragment with databinding and view model

--- app/src/main/java/com/pipe/d/dev/mvvmarchwine/favoriteModule/viewModel/FavoriteViewModel.kt
+++ app/src/main/java/com/pipe/d/dev/mvvmarchwine/favoriteModule/viewModel/FavoriteViewModel.kt
@@ -8,7 +8,9 @@ import com.pipe.d.dev.mvvmarchwine.R
 import com.pipe.d.dev.mvvmarchwine.common.entities.Wine
 import com.pipe.d.dev.mvvmarchwine.common.utils.Constants
 import com.pipe.d.dev.mvvmarchwine.favoriteModule.model.FavoriteRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class FavoriteViewModel(private val repository: FavoriteRepository): ViewModel() {
     private val _inProgress = MutableLiveData<Boolean>()
@@ -20,47 +22,59 @@ class FavoriteViewModel(private val repository: FavoriteRepository): ViewModel()
     private val _wines = MutableLiveData<List<Wine>>()
     val wines: LiveData<List<Wine>> = _wines
 
-    private  fun getAllWines() {
+    init {
+        getAllWines()
+    }
+
+    fun getAllWines() {
         viewModelScope.launch {
             _inProgress.value = Constants.SHOW
             try {
-                val result = repository.getAllWines()
+                withContext(Dispatchers.IO) {
+                    val result = repository.getAllWines()
 
-                result?.let {
-                    _wines.value  = result!!
-                } ?: {
-                    _snackBarMsg.value = R.string.room_request_fail
+                    result?.let {
+                        //_wines.value  = result!!
+                        _wines.postValue(result!!)
+                    } ?: {
+                        _snackBarMsg.postValue(R.string.room_request_fail)
+                    }
                 }
+
             }finally {
                 _inProgress.value = Constants.HIDE
             }
         }
     }
 
-    private fun addWine(wine: Wine) {
+    fun addWine(wine: Wine) {
         viewModelScope.launch {
             _inProgress.value = Constants.SHOW
             try {
-                val result = repository.addWine(wine)
-                if (result != -1L) {
-                    _snackBarMsg.value = R.string.room_save_fail
-                } else {
-                    _snackBarMsg.value = R.string.room_save_success
+                withContext(Dispatchers.IO) {
+                    val result = repository.addWine(wine)
+                    if (result != -1L) {
+                        _snackBarMsg.postValue(R.string.room_save_success)
+                    } else {
+                        _snackBarMsg.postValue(R.string.room_save_fail)
+                    }
                 }
             }finally {
                 _inProgress.value = Constants.HIDE
             }
         }
     }
-    private fun deleteWine(wine: Wine) {
+    fun deleteWine(wine: Wine) {
         viewModelScope.launch {
             _inProgress.value = Constants.SHOW
             try {
-                val result = repository.deleteWine(wine)
-                if (result == 0) {
-                    _snackBarMsg.value = R.string.room_save_fail
-                } else {
-                    _snackBarMsg.value = R.string.room_save_success
+                withContext(Dispatchers.IO) {
+                    val result = repository.deleteWine(wine)
+                    if (result == 0) {
+                        _snackBarMsg.postValue(R.string.room_save_fail)
+                    } else {
+                        _snackBarMsg.postValue(R.string.room_save_success)
+                    }
                 }
             }finally {
                 _inProgress.value = Constants.HIDE
